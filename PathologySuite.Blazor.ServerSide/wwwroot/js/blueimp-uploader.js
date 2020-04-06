b70e7cc1693e88e1d94599f37281a14591ac8d34
Christoph Neuner <christoph_neuner@web.de>
2020-04-07 00:40:11 +0200

    pause and resume functionality now working; but cancel function now no longer working

--- PathologySuite.Blazor.ServerSide/wwwroot/js/blueimp-uploader.js
+++ PathologySuite.Blazor.ServerSide/wwwroot/js/blueimp-uploader.js
@@ -1,5 +1,8 @@
 ﻿var filenameToDeleteUrl = {}
 var filenameToUploadedBytes = {}
+var filenameToProgress = {}
+//set value to true, if paused intentionally
+var filenameToIsPaused = {}
 
 window.InitUploader = () => {
     $('#fileupload').fileupload({
@@ -12,26 +15,25 @@ window.InitUploader = () => {
         //callbacks
         add: async function (e, data) {
             filename = data.files[0].name;
-            console.log('1 ' + filename)
-            filenameToUploadedBytes[filename] = 0;
+            filenameToUploadedBytes[filename] = filenameToProgress[filename] = 0;
             var jqXHR = null;
             data.context = $('<div/>').addClass('upload_file_wrapper').appendTo('#UploadList');
-            data.context.append('<p/>').addClass('upload_file_name').text(filename);
+            data.context.append($('<p/>').addClass('upload_file_name').text(filename));
             data.context.append($('<button/>').addClass('upload_button_individual').text('Upload')
                 .click(async function () {
-                    //if this is removed, the ui element disappears after data.submit()
+                    //if this is removed, an empty page will be shown after data.submit()
                     data.context.append($('<p/>').replaceAll($(this)));
                     jqXHR = data.submit();
 
-                    //for some reason, this needs to be updated here. otherwise, this will be name of the last added file, for all buttons
+                    //this needs to be updated here. otherwise, this will be the name of the last added file for all buttons
                     filename = data.files[0].name;
-                    console.log('2 ' + filename);
                     addCancelButton(jqXHR, data, filename);
+                    //addPauseButton(jqXHR, data, filename);
                 })
             );
 
-            data.context.append($('<progress/>').attr('value', 0).attr('max', 100).addClass('individual_progress_bar'));
-
+            addIndividualProgressBar(data, 0);
+       
             $("#button_upload_all").on("click", function () {
                 data.context.find(".upload_button_individual").click();
             })
@@ -46,9 +48,11 @@ window.InitUploader = () => {
         //},
 
         progress: function (e, data) {
+            filename = data.files[0].name;
             //var progress = parseInt((data.loaded / data.total) * 100, 10);
             var progress = (data.uploadedBytes / data.total) * 100
             data.context.find("progress").attr('value', progress);
+            filenameToProgress[filename] = progress;
         },
 
         chunkdone: async function (e, data) {
@@ -73,13 +77,51 @@ window.InitUploader = () => {
             //TODO make post request to server, that file has been completely uploaded and can be processed
 
         },
+        fail: async function (e, data) {
+            if (filenameToIsPaused[data.files[0].name]) {
+                var jqXHR = null;
+                data.context.append($('<button/>').addClass('resume_button_individual').text('resume upload')
+                    .click(async function () {
+                        data.context.append($('<p/>').replaceAll($(this)));
+                        filename = data.files[0].name;
+                        data.uploadedBytes = filenameToUploadedBytes[filename];
+                        jqXHR = data.submit();
+                        filenameToIsPaused[data.files[0].name] = false;
+                        filename = data.files[0].name;
+                        addPauseButton(jqXHR, data, filename);                       
+                    }))
+            }
+
+            //TODO: handle other reasons that could cause an interruption/abortion of the submission, this should mainly be a network error
+        }
     });
+}
+
+
+
+function addIndividualProgressBar(data, value) {
+    data.context.append($('<progress/>').attr('value', value).attr('max', 100).addClass('individual_progress_bar'));
+}
+
+
+function addPauseButton(jqXHR, data, filename) {
+    data.context.append($('<button/>').addClass('pause_button_individual').text('Pause upload')
+        .click(async function () {
+            filenameToIsPaused[data.files[0].name] = true;
+
+            //jqXHR.abort() triggers the "fail" callback
+            jqXHR.abort();
+            //this prevents abort() from removing ui elements
+            data.context.append($('<p/>').replaceAll($(this)));                                       
+        }))
 }
 
 async function addCancelButton(jqXHR, data, filename) {
     data.context.append($('<button/>').addClass('cancel_button_individual').text('Cancel').prop('disabled', false).click(async function () {
-        console.log(filename);
-        jqXHR.abort();
+        console.log(filenameToIsPaused[filename])
+        if (!filenameToIsPaused[filename]) {
+            jqXHR.abort();
+        }
         result = await ajaxDeleteRequest(filename)
         console.log('ajaxDeleteRequest: ' + result)
         if (result) {
@@ -94,7 +136,7 @@ async function addCancelButton(jqXHR, data, filename) {
 
 async function addDeleteButton(data, filename) {
     data.context.find('.cancel_button_individual').unbind().removeClass('cancel_button_individual').text('Delete from server').addClass('delete_button_individual').click(async function () {
-        data.context.detach()
+        data.context.remove()
         try {
             result = await ajaxDeleteRequest(filename)
             if (result) {
@@ -106,7 +148,7 @@ async function addDeleteButton(data, filename) {
             }
         }
         catch (e) {
-            console.log(e);
+            console.log('Exception in addDeleteButton' + e);
         }
     }).prop('disabled', false);
 }
@@ -131,7 +173,6 @@ async function ajaxDeleteRequest(filename) {
         return result['success'];
     }
     else {
-        console.log('here')
         return true;
     }
 }
@@ -184,3 +225,20 @@ function timeout(ms) {
                     //    //data.context.append($('<progress/>').attr('value', (data.uploadedBytes / data.total) * 100).attr('max', '100').addClass('individual_progress_bar'));
 
                     //}));
+
+
+
+                            //var children = data.context[0].children;
+                            //console.log(children);
+                            ////data.context = $('<div/>').addClass('upload_file_wrapper').appendTo('#UploadList');
+                            //var element_wrapper = document.createElement('div');
+                            //element_wrapper.className = 'upload_file_wrapper';
+                            //for (i = 0; i < children.length; i++) {
+                            //    //console.log(children[i]);
+                            //    //console.log(typeof (children[i]))
+                            //    element_wrapper.appendChild(children[i]);
+
+                            //}
+
+                            //document.getElementById('UploadList').appendChild(element_wrapper);
+                            //console.log(element_wrapper);
