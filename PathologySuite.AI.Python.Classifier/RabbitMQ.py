4e5e97410d2d0abd4dbeb08fb74a39483bd5a618
christoph_neuner@web.de <christoph_neuner@web.de>
2020-05-12 16:32:18 +0200

    working example: user initiates ai processing in gui and sends message to python app via rabbitmq -> python app predicts  and sends message out with preds -> background task receives preds via messaging

--- PathologySuite.AI.Python.Classifier/RabbitMQ.py
+++ PathologySuite.AI.Python.Classifier/RabbitMQ.py
@@ -1,50 +1,54 @@
 import pika
+import time
+import pga_entity_classifier
+import pathlib
+from pathlib import Path
 
-q_name_work = "pituitary_gland_entity_classification_work";
-q_name_preds = "pituitary_gland_entity_classification_preds";
+q_name_work = "pituitary_gland_adenomas_entity_classification_new_work";
 connection_address = 'localhost'
-
-
-
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(connection_address))
-channel = connection.channel()
-channel.queue_declare(queue=q_name_preds)
-response = 'ACTH: 10%; LH: 95%'
-channel.basic_publish(exchange='',
-                  routing_key=q_name_preds,
-                  body=response)
-print(f'[x] Sent {response}')
-connection.close()
-
-
-
 
 
 ### do work and send response
 def callback(ch, method, properties, body):
-    #work
-    print(" [x] Received %r" % body)
 
+    path_wsi = body.decode()
+
+    print(f" [x] Received {path_wsi}")
+    
+    ###
+    # make predictions
+    ###
+    path_exported_learner = pathlib.Path('D:/FAUbox/Projects/PathologySuite/PathologySuite.AI.Python.Classifier/n_13-iteration_4-weights_bestmodel_lr2=1e-7--lr3=1e-6_9.pkl')  
+    preds = pga_entity_classifier.predict_pga_entity(path_exported_learner, path_wsi)
+    
+    ###
     #send response
+    ###
+    response = str(preds)
+
     connection = pika.BlockingConnection(pika.ConnectionParameters(connection_address))
-    channel = connection.channel()
-    channel.queue_declare(queue=q_name_preds)
-    response = 'ACTH: 10%; LH: 95%'
-    channel.basic_publish(exchange='',
-                      routing_key=q_name_preds,
-                      body=response)
+    channel = connection.channel()    
+    channel.basic_qos(prefetch_count=1) #Fair dispatch
+    properties = pika.BasicProperties(delivery_mode = 2,) # make message persistent
+    channel.basic_publish(exchange='PathologySuite.AI',
+                      routing_key='multiLabelClassification.pituitaryAdenomas.entities.prediction',
+                      body=response, 
+                      properties=properties) 
     print(f'[x] Sent {response}')
     connection.close()
+    
+    ch.basic_ack(delivery_tag = method.delivery_tag) #Manual message acknowledgment
 
 
 ### receive work
 connection = pika.BlockingConnection(pika.ConnectionParameters(connection_address))
 channel = connection.channel()
-channel.queue_declare(queue=q_name_work)
-channel.basic_consume(queue=q_name_work,
-                      auto_ack=True,
-                      on_message_callback=callback)
+#Fair dispatch
+channel.basic_qos(prefetch_count=1)
+
+channel.queue_declare(queue=q_name_work, durable=True, auto_delete=False)
+channel.queue_bind(exchange='PathologySuite.AI', queue=q_name_work, routing_key='multiLabelClassification.pituitaryAdenomas.entities.newWork')
+channel.basic_consume(queue=q_name_work, on_message_callback=callback, auto_ack=False)
 
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
--- RabbitMQReceiver/Program.cs
+++ RabbitMQReceiver/Program.cs
@@ -9,19 +9,15 @@ namespace RabbitMQReceiver
     {
         static void Main(string[] args)
         {
-            string q_name_work = "pituitary_gland_entity_classification_work";
-            string q_name_preds = "pituitary_gland_entity_classification_preds";
+            string q_name_preds = "pituitary_gland_adenomas_entity_classification_predictions";
             string connection_address = "localhost";
 
             var factory = new ConnectionFactory() { HostName = connection_address };
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: q_name_preds,
-                                     durable: false,
-                                     exclusive: false,
-                                     autoDelete: false,
-                                     arguments: null);
+                channel.QueueDeclare(queue: q_name_preds, durable: true, exclusive: false, autoDelete: false, arguments: null);
+                channel.QueueBind(queue: q_name_preds, exchange: "PathologySuite.AI", routingKey: "multiLabelClassification.pituitaryAdenomas.entities.prediction");
 
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
@@ -29,9 +25,14 @@ namespace RabbitMQReceiver
                     var body = ea.Body;
                     var message = Encoding.UTF8.GetString(body.ToArray());
                     Console.WriteLine(" [x] Received {0}", message);
+
+                    // Manual message acknowledgment
+                    // Note: it is possible to access the channel via
+                    //       ((EventingBasicConsumer)sender).Model here
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
                 };
                 channel.BasicConsume(queue: q_name_preds,
-                                     autoAck: true,
+                                     autoAck: false,
                                      consumer: consumer);
 
                 Console.WriteLine(" Press [enter] to exit.");
